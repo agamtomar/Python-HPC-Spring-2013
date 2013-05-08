17a916001182da048086effd663ca6ba950e40cc
Thomas Hauser <thomas@thehausers.net>
2013-05-08 08:58:40 -0600

    Fixed the tutorial for extending python

--- 10_extend/05_f2py_heat.py
+++ 10_extend/05_f2py_heat.py
@@ -1,21 +1,22 @@
-from numpy import zeros
+from numpy import zeros, asfortranarray
 from matplotlib import pyplot as P
 from cProfile import run
 from pstats import Stats
 
-from f2py_update import f2py_update
+from f2py_update import f2py_update90
 
 dx = 0.1
 dy = 0.1
 dx2 = dx*dx
 dy2 = dy*dy
 n = 300
-niter=800
+niter=10000
 profile = '05_f2py_profile.dat'
 
-def calc(N, Niter=100, func=f2py_update, args=(dx2, dy2)):
+def calc(N, Niter=100, func=f2py_update90, args=(dx2, dy2)):
     u = zeros([N, N])
-    u[0] = 1
+    u[0] = 1.0
+    u = asfortranarray(u)
     for i in range(Niter):
         func(u,*args)
     return u
--- 10_extend/05_f2py_update.f90
+++ 10_extend/05_f2py_update.f90
@@ -1,6 +1,6 @@
 ! fortran 90 update
 
-subroutine f2py_update(np, u, dx2, dy2)
+subroutine f2py_update90(np, u, dx2, dy2)
     integer, intent(in) :: np
     double precision, intent(inout) :: u(0:np-1, 0:np-1)
     double precision, intent(in) :: dx2, dy2
@@ -11,6 +11,6 @@ subroutine f2py_update(np, u, dx2, dy2)
     double precision :: dnr_inv
     
     dnr_inv = 0.5d0 / (dx2 + dy2)
-
+    
     u(1:np-2, 1:np-2) = ((u(0:np-3, 1:np-2) + u(2:np-1, 1:np-2)) * dy2 + (u(1:np-2, 0:np-3) + u(1:np-2, 2:np-1)) * dx2) * dnr_inv
-end subroutine f2py_update                  
+end subroutine f2py_update90                  
